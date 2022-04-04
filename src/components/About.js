55cf38df19f7ff0accc92515838f1dd75d95f840
Lindsey <linlidiaramirez@gmail.com>
2022-04-04 15:08:31 -0400

    updated title text

--- src/components/About.js
+++ src/components/About.js
@@ -7,6 +7,7 @@ export default function About() {
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             
               <div class="flex font-link-bold flex-col">
+              <ul class="text-4xl text-white font-semibold pb-3">Hola, I'm</ul>
               <h1
               class="font-bold text-6xl md:text-8xl"
               >Lindsey</h1>
@@ -15,13 +16,13 @@ export default function About() {
               </div>
               <br className="hidden lg:inline-block" />
               
-              <div class="text-white">
+              <div class="text-white pt-12">
              <ul class="text-4xl font-semibold pb-3">I Engineer</ul>
              <a href="#projects" class="hover:text-white">
-                <li class="list-none text-xl font-extralight">_Software (Sites +/ Apps)</li>
+                <li class="list-none text-xl font-extralight">_Code (Sites +/ Apps)</li>
                 
       
-                <li class="list-none text-xl font-extralight">_Digital products (Infographics)</li>
+                <li class="list-none text-xl font-extralight">_UI/UX (Infographics)</li>
                 <li class="list-none text-xl font-extralight">_E-commerce + Crypto payments </li>
                 </a>
                 <ul class="text-4xl font-semibold pb-3 pt-6">I Write</ul>
@@ -33,7 +34,7 @@ export default function About() {
                 </a>
                 <ul class="text-4xl font-semibold pb-3 pt-6">I Travel</ul>
                 <a href="#travel" class="hover:text-white">
-                <li class="list-none text-xl font-extralight">_Wax on, Wax off of life</li> 
+                <li class="list-none text-xl font-extralight">_Wax on, Wax off</li> 
                 </a>
                 <a href="#photos" class="hover:text-white">
                   <li class="list-none text-xl font-extralight">_Model w <a href="https://www.statemgmt.com/new-york/lifestyle/women/all/1596959" target="_blank">StateMGMT NYC</a> </li>
