c5f1eaa02cf5353dc0345d8bbbde903c3c0217e2
Lindsey <linlidiaramirez@gmail.com>
2022-05-12 23:59:21 -0400

    added green blink animation

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ export default function About() {
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             
               <div class="flex font-link-bold flex-col">
-              <ul class="text-4xl text-white font-semibold pt-6 pb-3 md:pt-0">Hola, I'm</ul>
+              <ul class="text-4xl text-yahGreen font-semibold pt-6 pb-3 md:pt-0">Hola, I'm</ul>
               <h1
               class="font-bold text-6xl md:text-8xl"
               >Lindsey</h1>
@@ -19,25 +19,23 @@ export default function About() {
               <div class="text-white pt-12">
              <ul class="text-4xl font-semibold pb-3">I Engineer</ul>
              <a href="#projects" class="hover:text-white">
-                <li class="list-none text-xl font-extralight">_Code (Sites +/ Apps)</li>
-                
-      
-                <li class="list-none text-xl font-extralight">_UI/UX (Infographics)</li>
-                <li class="list-none text-xl font-extralight">_E-commerce</li>
+                <li class="list-none text-xl font-extralight"><span class="blinking">_</span>Code (Sites +/ Apps)</li>
+                <li class="list-none text-xl font-extralight"><span class="blinking">_</span>UI/UX (Infographics)</li>
+                <li class="list-none text-xl font-extralight"><span class="blinking">_</span>E-commerce</li>
                 </a>
                 <ul class="text-4xl font-semibold pb-3 pt-6">I Write</ul>
                 <a href="https://www.cweonline.org/">
-                <li class="list-none text-xl font-extralight">_Consultant </li>
+                <li class="list-none text-xl font-extralight"><span class="blinking">_</span>Consultant </li>
                 </a>
                 {/* <a href="blog" class="hover:text-pink">
                 <li class="list-none text-xl font-extralight">_Blog, Media </li> 
                 </a> */}
                 <ul class="text-4xl font-semibold pb-3 pt-6">I Travel</ul>
                 <a href="#travel" class="hover:text-white">
-                <li class="list-none text-xl font-extralight">_Wax on, Wax off</li> 
+                <li class="list-none text-xl font-extralight"><span class="blinking">_</span>Wax on, Wax off</li> 
                 </a>
                 <a href="#travel" class="hover:text-white">
-                  <li class="list-none text-xl font-extralight">_Model</li>
+                  <li class="list-none text-xl font-extralight"><span class="blinking">_</span>Model</li>
                 </a>
                 </div>
                
@@ -53,3 +51,4 @@ export default function About() {
       </section>
     );
 }
+
